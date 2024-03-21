aaf5e9956b6cb5bbb0f60c18f53ba42441bc0be9
Noel Bundy <twystneko@gmail.com>
2024-03-21 01:00:50 -0600

    New Features! -aco, -txt, -hex

--- ASEtoACO.py
+++ ASEtoACO.py
@@ -2,40 +2,76 @@
 
 import os
 import struct
+import sys
 from pathlib import Path
 
-directory = os.path.join(os.path.dirname(__file__),"palettes/")
+# commandline args: -aco (or blank) for .ACO output, -png for 8x8px swatches, -txt for text, -hex for hexcode list
+try:
+    cmd = sys.argv[1]
+except:
+    cmd = "-aco"
+
+
+class Color:
+    def __init__(self,name,red,green,blue) -> None:
+        self.name = name.decode("UTF-16BE")[:-1]
+        self.rawname = name
+        self.red = int(red*255)
+        self.float_red = red
+        self.green = int(green*255)
+        self.float_green = green
+        self.blue = int(blue*255)
+        self.float_blue = blue
+
+directory = os.path.join(os.path.dirname(__file__),"palettes\\")
 print("Looking for ASE files in "+ directory)
 for file in os.listdir(directory):
     pal_file = os.path.join(directory,file)
-
     if pal_file.endswith(".ase"):
-        
-        outfile = os.path.join(directory,Path(pal_file).stem + ".aco")
- 
+        filename = Path(pal_file).stem
+        #reading ASE file
         pf = open(pal_file,"rb")
-        of = open(outfile, "wb")
-        
-        header_chunk = pf.read(12) #header is 12 bytes: 4 / 2 / 2 / 4. 
-        header, majver, minver, total_blocks = struct.unpack_from('>4shhi', header_chunk)
-        aco_header = struct.pack('>hh',1,total_blocks)
-        of.write(aco_header)
-
+        header, majver, minver, total_blocks = struct.unpack_from('>4shhi', pf.read(12))
+        palette = []
         for block in range(total_blocks):
-            block_header_chunk = pf.read(6) #block header is 6 bytes: id, blocklen
-            block_id, block_length= struct.unpack_from('>hi', block_header_chunk)
+            block_id, block_length= struct.unpack_from('>hi', pf.read(6))
             block_data = pf.read(block_length)
             if block_id == 1: #it's a color, we want it!
                 name_len = struct.unpack_from('>h',block_data[:2])
-                n_len = name_len[0]*2
-                format_string = '>h' + str(n_len)+'s4sfffh'
-                name_len, name, color_space,red,green,blue,mode  = struct.unpack_from(format_string, block_data)
-                out_len = name_len - 1
-                red = int(red * 255)
-                green = int(green * 255)
-                blue = int(blue * 255)
-                aco_color = struct.pack('>hBBBBBBh', 0,red,red,green, green, blue, blue, 0)
-                of.write(aco_color)
+                name_len, name, color_space,red,green,blue,mode  = struct.unpack_from('>h' + str(name_len[0] *2)+'s4sfffh', block_data)
+                c = Color(name,red,green,blue)
+                palette.append(c)
         pf.close()
-        of.close()
-        print("Converted "+ Path(pal_file).stem + " to .ACO format.")
+
+        #decide what to output
+        if cmd == "-aco":
+            #writing ACO file
+            format = "ACO"
+            outfile = os.path.join(directory, filename + ".aco")
+            of = open(outfile, "wb")
+            of.write(struct.pack('>hh',1,len(palette)))
+            for c in palette:
+                of.write(struct.pack('>hBBBBBBh', 0,c.red,c.red,c.green, c.green, c.blue, c.blue, 0))
+            of.close()
+        elif cmd == "-txt":
+            #writing text file: Name, R, G, B
+            format = "TXT"
+            outfile = os.path.join(directory, filename + ".txt")
+            of = open(outfile, "w")
+            of.write(";Palette Name: "+filename+" -- "+str(len(palette))+" colors.\n")
+            for c in palette:
+                of.write(c.name + ": "+str(c.red)+","+str(c.green)+","+str(c.blue)+"\n")
+            of.close()
+        elif cmd == "-hex":
+            #writing hex file: colors in HEX format. 
+            format = "HEX"
+            outfile = os.path.join(directory, filename + ".hex")
+            of = open(outfile, "w")
+            of.write(";Palette Name: "+filename+" -- "+str(len(palette))+" colors.\n")
+            for c in palette:
+                of.write("#"+f"{c.red:0{2}X}")
+                of.write(f"{c.green:0{2}X}")
+                of.write(f"{c.blue:0{2}X}"+"\n")
+            of.close()
+        
+        print("Converted "+ Path(pal_file).stem + " to "+format+" format.  " + str(len(palette)) + " colors in palette")
